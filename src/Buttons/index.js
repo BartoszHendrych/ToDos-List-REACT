898d3909df5b8a42b4c02f29a4ba53e0e9400061
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-07-12 22:27:12 +0200

    Task list without interactions yet

---
+++ src/Buttons/index.js
@@ -0,0 +1,22 @@
+import "./style.css";
+
+const Buttons = ({ tasks, hideDoneTasks }) => (
+        <div className="button">
+            {tasks.length > 0 && (
+                <>
+                    <button className="button__buttons">
+                        {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                    </button>
+                    <button 
+                        className="button__buttons"
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
+                    </button>
+                </>
+            )}
+        </div>   
+);
+
+
+export default Buttons;
