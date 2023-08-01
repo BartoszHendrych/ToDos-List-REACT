6b26daa50f345bb9137a91d387c8eb8d4a0d5dcd
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-08-01 22:12:49 +0200

    Added and modyfication in the file section

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,2 +1,25 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.section`
+    background-color: hsl(0, 0%, 100%);
+    margin: 20px auto;
+`;
+
+export const Header = styled.header`
+    display: grid;
+    grid-template-columns: 1fr auto auto;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        grid-template-columns: 1fr;
+    }
+`;
+
+export const Title = styled.h2`
+    padding: 20px;
+    margin: 0;
+`;
+
+export const Body = styled.div`
+    border-top: 1px solid hsl(0, 0%, 80%);
+    padding: 20px;
+`;
