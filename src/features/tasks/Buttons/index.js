f8e2a6003a68722cc19b6fa722c9165b0b27b980
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-08-15 14:42:23 +0200

    tasks in the file button is a array

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -3,9 +3,11 @@ import { Wrapper, Button } from "./styled";
 import { selectTasks, toggleHideDone, selectHideDone, setAllDone } from "../taskSlice";
 
 const Buttons = () => {
-    const tasks = useSelector(selectTasks);
+    const { tasks } = useSelector(selectTasks);
     const hideDone = useSelector(selectHideDone);
+
     const dispatch = useDispatch();
+    
     return (
         <Wrapper>
             {tasks.length > 0 && (
